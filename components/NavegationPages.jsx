01e34948be12aa52d20fb261d6d6d99e984f442c
waelkamira <127122203+waelkamira@users.noreply.github.com>
2025-02-16 03:05:05 +0300

    main search

--- components/NavegationPages.jsx
+++ components/NavegationPages.jsx
@@ -7,15 +7,16 @@ import {
 } from 'react-icons/md';
 
 export default function NavegationPages({ array, setPageNumber, pageNumber }) {
+  console.log('array', array);
   return (
-    <div className="flex items-center justify-around  mt-4">
+    <div className="flex items-center justify-around  mt-4 text-white">
       {array?.length >= 5 && (
         <Link href={'#post1'}>
           <div
             className="flex items-center justify-around cursor-pointer py-4"
             onClick={() => setPageNumber(pageNumber + 1)}
           >
-            <h1 className="">الصفحة التالية</h1>
+            <h1 className="text-white">الصفحة التالية</h1>
             <MdKeyboardDoubleArrowRight className="text-2xl text-one select-none" />
           </div>
         </Link>
@@ -27,7 +28,7 @@ export default function NavegationPages({ array, setPageNumber, pageNumber }) {
             onClick={() => setPageNumber(pageNumber - 1)}
           >
             <MdKeyboardDoubleArrowLeft className="text-2xl text-one select-none" />
-            <h1 className="">الصفحة السابقة</h1>
+            <h1 className="text-white">الصفحة السابقة</h1>
           </div>
         </Link>
       )}
