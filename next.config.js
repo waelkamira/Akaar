6b9e6def87041728234efa7b465782cc5f8a3db9
waelkamira <127122203+waelkamira@users.noreply.github.com>
2025-03-17 00:48:30 +0300

    fix categories

--- next.config.js
+++ next.config.js
@@ -12,70 +12,70 @@ const nextConfig = {
         protocol: 'https',
         hostname: '**', // السماح بجميع النطاقات
       },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'scontent-ord5-1.xx.fbcdn.net',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'scontent-lga3-1.xx.fbcdn.net',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'scontent-hou1-1.xx.fbcdn.net',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'scontent-ord5-2.xx.fbcdn.net',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'scontent-msp1-1.xx.fbcdn.net',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'scontent-lga3-2.xx.fbcdn.net',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'res.cloudinary.com',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'ik.imagekit.io',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'i.ibb.co',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'via.placeholder.com',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'lh3.googleusercontent.com',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'i.imgur.com',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'www.allrecipes.com',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'kitchen.sayidaty.net',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'cooking-ways.com',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'v-genius.fatafeat.com',
-      // },
+      {
+        protocol: 'https',
+        hostname: 'scontent-ord5-1.xx.fbcdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent-lga3-1.xx.fbcdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent-hou1-1.xx.fbcdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent-ord5-2.xx.fbcdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent-msp1-1.xx.fbcdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent-lga3-2.xx.fbcdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.allrecipes.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'kitchen.sayidaty.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cooking-ways.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'v-genius.fatafeat.com',
+      },
     ],
   },
 };
