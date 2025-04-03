72db072565bbacb1d37faabaaeb94d7d5c713a1e
waelkamira <127122203+waelkamira@users.noreply.github.com>
2025-04-03 19:46:01 +0300

    good

--- components/ReusableComponents/NavegationPages.jsx
+++ components/ReusableComponents/NavegationPages.jsx
@@ -2,8 +2,6 @@
 import { motion } from 'framer-motion';
 import React from 'react';
 import {
-  MdKeyboardArrowLeft,
-  MdKeyboardArrowRight,
   MdKeyboardDoubleArrowLeft,
   MdKeyboardDoubleArrowRight,
 } from 'react-icons/md';
@@ -13,7 +11,6 @@ export default function NavegationPages({
   setPageNumber,
   pageNumber,
 }) {
-  // console.log('array', array);
   return (
     <motion.div
       whileHover={{ scale: 1.03 }}
@@ -44,46 +41,5 @@ export default function NavegationPages({
         </motion.button>
       )}
     </motion.div>
-    // <motion.div
-    //   whileHover={{ scale: 1.03 }}
-    //   whileTap={{ scale: 0.95 }}
-    //   className="flex items-center justify-around gap-4 my-16"
-    // >
-    //   {hasMore && (
-    //     <motion.button
-    //       whileHover={{ scale: 1.05 }}
-    //       whileTap={{ scale: 0.95 }}
-    //       className="flex items-center justify-center text-sm sm:text-lg gap-2 py-2 px-2 sm:py-3 sm:px-8
-    //   bg-transparent
-    //   hover:bg-primary-500/10
-    //   rounded-xl shadow-lg hover:shadow-xl
-    //   text-white font-semibold
-    //   border-2 border-primary-500/20 hover:border-primary-500/50
-    //   transition-all duration-300"
-    //       onClick={() => setPageNumber(pageNumber + 1)}
-    //     >
-    //       الصفحة التالية
-    //       <MdKeyboardDoubleArrowRight className="text-xl text-primary-500 animate-pulse" />
-    //     </motion.button>
-    //   )}
-
-    //   {pageNumber > 1 && (
-    //     <motion.button
-    //       whileHover={{ scale: 1.05 }}
-    //       whileTap={{ scale: 0.95 }}
-    //       className="flex items-center justify-center text-sm sm:text-lg gap-2 py-2 px-2 sm:py-3 sm:px-8
-    //   bg-transparent
-    //   hover:bg-primary-500/10
-    //   rounded-xl shadow-lg hover:shadow-xl
-    //   text-white font-semibold
-    //   border-2 border-primary-500/20 hover:border-primary-500/50
-    //   transition-all duration-300"
-    //       onClick={() => setPageNumber(pageNumber - 1)}
-    //     >
-    //       <MdKeyboardDoubleArrowLeft className="text-xl text-primary-500 animate-pulse" />
-    //       الصفحة السابقة
-    //     </motion.button>
-    //   )}
-    // </motion.div>
   );
 }
