7f54847ebfa218a61d972498dca9d59381a43de1
waelkamira <127122203+waelkamira@users.noreply.github.com>
2025-04-05 02:52:25 +0300

    fix errors

--- components/navbars/MainNavbar.jsx
+++ components/navbars/MainNavbar.jsx
@@ -2,16 +2,15 @@
 import FirstNavBar from './FirstNavBar';
 import Search from '../Search/Search';
 import Hero from '../Home/Hero';
-import { SearchProvider } from '../../contexts/SearchContext';
 
 export default function MainNavbar() {
   return (
     <div className="relative flex flex-col justify-center items-center w-full inset-0 mb-20">
       <FirstNavBar />
+      <Search />
       <div className="w-full sm:h-96">
         <Hero />
       </div>
-      <Search />
     </div>
   );
 }
